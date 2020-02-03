0602849440a51b2988e1b63f53d15635c53cf761
Branislav Hesko <branislav.hesko@protonmail.ch>
2020-02-03 23:19:16 +0100

    Minor fixes

--- predict.py
+++ predict.py
@@ -19,32 +19,33 @@ class Predictor:
         self._load_model(weights_path)
         self._image_loader = ImageLoader((self._config.CROP_SIZE, self._config.CROP_SIZE), self._config.STRIDE)
 
+    @torch.no_grad()
     def predict(self, image):
         assert len(image.shape) == 3, "Image should be in RGB format for now."
+        mask = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) > 0
         self._image_loader.set_image_and_mask(image, image[:, :, 0])
         indices = self._image_loader.get_indices()
         count_map = np.zeros(image.shape[:2])
         prediction = torch.zeros((self._config.NUM_CLASSES, *image.shape[:2]))
         if self._cuda:
             prediction = prediction.cuda()
-        with torch.no_grad():
-            for idx in tqdm(indices):
-                input_slice = image[idx[0]: idx[2], idx[1]: idx[3], :]
-                input_slice = self._transform(input_slice, input_slice[:, :, 0])[0]
-                if self._cuda:
-                    input_slice = input_slice.cuda()
-                output = self.model(input_slice.unsqueeze(dim=0))
-                prediction[:, idx[0]: idx[2], idx[1]: idx[3]] += torch.squeeze(output.data)
-                count_map[idx[0]: idx[2], idx[1]: idx[3]] += 1
+        for idx in tqdm(indices):
+            input_slice = image[idx[0]: idx[2], idx[1]: idx[3], :]
+            input_slice = self._transform(input_slice, input_slice[:, :, 0])[0]
+            if self._cuda:
+                input_slice = input_slice.cuda()
+            output = self.model(input_slice.unsqueeze(dim=0))
+            prediction[:, idx[0]: idx[2], idx[1]: idx[3]] += torch.squeeze(output.data)
+            count_map[idx[0]: idx[2], idx[1]: idx[3]] += 1
 
         prediction = prediction.cpu().numpy() / count_map
-        return np.argmax(prediction, axis=0)
+        return np.argmax(prediction, axis=0) & mask
 
     def _load_model(self, weights_path: str):
         self.model = available_models[self._config.MODEL](self._config.NUM_CLASSES)
+        self.model.load_state_dict(torch.load(weights_path, map_location="cpu"))
         if self._cuda:
             self.model = self.model.cuda()
-        self.model.load_state_dict(torch.load(weights_path, map_location="cpu"))
 
 if __name__ == "__main__":
     import cv2
